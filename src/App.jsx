37db65e6f2c73c28d32fa2e89565da1ce0e51724
rokiseydi <roki.seydi@gmail.com>
2020-06-29 15:40:12 +0100

    changes to repo

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+import React, { Component } from 'react';
+import logo from './logo.svg';
+import styles from './App.module.scss';
+
+
+
+class App extends Component {
+  render() {
+    return (
+      <div>This is my component.</div>
+    );
+  }
+}
+
+export default App;
