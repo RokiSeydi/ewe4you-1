97dabcd551d75db2a418d7836024afa69009cffd
rokiseydi <roki.seydi@gmail.com>
2020-06-29 17:11:29 +0100

    changes in test and app.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,5 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
-import styles from './App.module.scss';
-
-
+//import styles from './App.module.scss';
 
 class App extends Component {
   render() {
