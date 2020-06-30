819668d328707fac13b92d60ce4518c776ad9615
Aqeel Yamin <aqeel@Aqeels-MacBook-Pro.local>
2020-06-30 16:35:21 +0100

    initial topic cards design

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,23 @@
 import React, { Component } from 'react';
+import styles from "./App.module.scss";
+import TopicCard from "./Components/TopicCard";
+import image from "./img/ewe4you.jpg";
 //import styles from './App.module.scss';
 
 class App extends Component {
   render() {
     return (
-      <div>This is my component.</div>
+    <>
+    <div className={styles.Flexed}>
+      <TopicCard />
+      <TopicCard />
+      <TopicCard />
+    </div>
+    {image}
+    </>
+    
+  
+  
     );
   }
 }
