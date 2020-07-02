b702954320b0deafe123e666d7fd6bc5d0da063e
rokiseydi <roki.seydi@gmail.com>
2020-07-02 14:47:32 +0100

    tests fixed

--- src/Components/ToolCard/ToolCard.jsx
+++ src/Components/ToolCard/ToolCard.jsx
@@ -2,13 +2,14 @@ import React, { Component } from 'react';
 import styles from "./ToolCard.module.scss";
 import TopicCard from "../TopicCard";
 
-class ToolCard extends TopicCard {
+class ToolCard extends Component {
   
   render () {
    
     return (
-      
-    );
+      <p>toolcard works</p>
+    )
+    
   }
 }
 
