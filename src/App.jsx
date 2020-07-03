3d2fb0a0ed5d306c5f75188f8e1b52ca7dcfe353
Shane Wilson <65076931+shanewils@users.noreply.github.com>
2020-07-03 15:32:19 +0100

    fixed?

--- src/App.jsx
+++ src/App.jsx
@@ -2,10 +2,6 @@ import React, { Component } from 'react';
 import styles from './App.module.scss';
 import Routes from "./Routes/Routes";
 
-<<<<<<< HEAD
-=======
-
->>>>>>> 653741c264302d516d5013dffdce0f77c15960e5
 class App extends Component {
   
   render() {
--- src/Routes/Routes.jsx
+++ src/Routes/Routes.jsx
@@ -1,19 +1,13 @@
 import React, { Component } from "react";
 import { Router } from "@reach/router";
-<<<<<<< HEAD
 import LandingPage from "../Components/LandingPage/LandingPage";
 import Dashboard from "../Components/Dashboard/Dashboard/Dashboard";
 import CardCarousel from "../Components/CardCarousel/CardCarousel";
 import TopicOverview from "../Components/TopicOverview/TopicOverview";
 import topics from "../data/topics";
-=======
-
-import LandingPage from "../Components/LandingPage/LandingPage"
-import Dashboard from "../Components/Dashboard/Dashboard/Dashboard"
 import ToolPage from "../containers/ToolPage";
 import TopicPage from "../containers/TopicPage";
-import CardCarousel from "../Components/CardCarousel/CardCarousel"
->>>>>>> 653741c264302d516d5013dffdce0f77c15960e5
+
 
 class Routes extends Component {
   render() {
@@ -21,14 +15,11 @@ class Routes extends Component {
       <Router>
         <LandingPage path="/" />
         <Dashboard path="dashboard" />
-<<<<<<< HEAD
         <CardCarousel path="card-carousel"/> 
         <TopicOverview path="topic-overview" topics={topics[0]} />
-=======
         <ToolPage path="tools" />
         <TopicPage path="topics" />   
-        <CardCarousel path="CardCarousel"/>
->>>>>>> 653741c264302d516d5013dffdce0f77c15960e5
+      
       </Router>
     );
   }
