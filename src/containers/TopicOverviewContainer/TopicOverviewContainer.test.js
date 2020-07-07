f188468c79075ffaf415164b60f07367d2d922e0
Oli Black <oli.black@nology.io>
2020-07-07 18:49:09 +0100

    added style changes in new branch from master

---
+++ src/containers/TopicOverviewContainer/TopicOverviewContainer.test.js
@@ -0,0 +1,8 @@
+import React from 'react';
+import { render } from '@testing-library/react';
+import TopicOverviewContainer from './TopicOverviewContainer';
+
+
+test('should render', () => {
+    expect(render( < TopicOverviewContainer / > )).toBeTruthy();
+});
