e718c09d22a843a4cb608b678f48b87e1aa8bd7c
rokiseydi <roki.seydi@gmail.com>
2020-07-07 12:31:48 +0100

    navbar opens and closes

--- src/Components/NavigationBar/NavigationBar.jsx
+++ src/Components/NavigationBar/NavigationBar.jsx
@@ -3,19 +3,26 @@ import styles from "./NavigationBar.module.scss";
 import logo from "../../img/logo.jpg";
 import { library } from '@fortawesome/fontawesome-svg-core'
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
-import { faTachometerAlt, faComments, faWrench, faUser, faPlayCircle } from '@fortawesome/free-solid-svg-icons'
+import { faTachometerAlt, faComments, faWrench, faUser, faPlayCircle } from '@fortawesome/free-solid-svg-icons';
 import { fab } from '@fortawesome/free-brands-svg-icons';
-// import Dashboard from "../Dashboard/Dashboard/Dashboard";
 import { Link } from '@reach/router';
 
 library.add(faTachometerAlt, faComments, faWrench, faUser, fab, faPlayCircle)
 
 class NavigationBar extends Component{
+
+state = {
+  isOpen: false
+}
+
+iconClick = () => {
+  this.setState({ isOpen: !this.state.isOpen});
+}
+
   render() {
-      return (
-        <>
-        <nav>
-            <img src={logo} alt="sheep logo"/>
+   const display = this.state.isOpen ?   
+  
+     ( <nav>    
           <div className={styles.links}>
             <p><span><FontAwesomeIcon icon="tachometer-alt"></FontAwesomeIcon><Link to="/dashboard">Dashboard</Link></span></p>
             <p><span><FontAwesomeIcon icon="comments"></FontAwesomeIcon><Link to="/topic-overview">Topics</Link></span></p>
@@ -23,10 +30,28 @@ class NavigationBar extends Component{
           </div>
           <div className={styles.account}>
             <p><span><FontAwesomeIcon icon="user"></FontAwesomeIcon><Link to="/">My Account</Link></span></p>
+          </div> 
+       </nav>) 
+
+         : null;
+   
+      return (
+        <>
+        <nav> 
+            <img onClick={this.iconClick} src={logo} alt="sheep logo"/>            
+              {display}
+          {/* <div className={styles.links}>
+            <p><span><FontAwesomeIcon icon="tachometer-alt"></FontAwesomeIcon><Link to="/dashboard">Dashboard</Link></span></p>
+            <p><span><FontAwesomeIcon icon="comments"></FontAwesomeIcon><Link to="/topic-overview">Topics</Link></span></p>
+            <p><span><FontAwesomeIcon icon="wrench"></FontAwesomeIcon><Link to="/tools">Toolbox</Link></span></p>
           </div>
-        </nav>
-        </>
-      );
+          <div className={styles.account}>
+            <p><span><FontAwesomeIcon icon="user"></FontAwesomeIcon><Link to="/">My Account</Link></span></p>
+          </div>
+       */}
+      </nav>
+    </>
+    );
   }
 }
 export default NavigationBar;
