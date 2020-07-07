96dbcb8e2aa8d7db139ec443c296c861d566a64f
Shane Wilson <65076931+shanewils@users.noreply.github.com>
2020-07-07 12:58:13 +0100

    Lint fixed

--- src/components/TopicCard/TopicCard.test.js
+++ src/components/TopicCard/TopicCard.test.js
@@ -1,7 +1,8 @@
 import React from 'react';
 import { render } from '@testing-library/react';
 import TopicCard from './TopicCard';
+import topics from "../../data/topics"
 
 test('should render', () => {
-    expect(render( < TopicCard /> )).toBeTruthy();
+    expect(render( < TopicCard topic={topics[0]}/> )).toBeTruthy();
 });
--- src/components/TopicOverview/TopicOverview.test.js
+++ src/components/TopicOverview/TopicOverview.test.js
@@ -1,7 +1,8 @@
 import React from 'react';
 import { render } from '@testing-library/react';
 import TopicOverview from './TopicOverview';
+import topics from "../../data/topics"
 
 test('should render', () => {
-    expect(render( < TopicOverview />)).toBeTruthy();
+    expect(render( < TopicOverview topic={topics[0]}/>)).toBeTruthy();
 });
