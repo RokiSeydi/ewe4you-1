aae760617cde98b229b0a1216e3132c48b62a843
Will Kirkpatrick <will.kirkpatrick@nology.io>
2020-07-07 16:40:46 +0100

    fixed merge issues

--- src/components/CardCarousel/CardCarousel.test.js
+++
@@ -1,9 +0,0 @@
-import React from 'react';
-import { render } from '@testing-library/react';
-import CardCarousel from './CardCarousel';
-import tools from "../../data/tools";
-import topics from "../../data/topics"
-
-test('should render', () => {
-    expect(render( < CardCarousel cardType={'tools'} /> )).toBeTruthy();
-});
