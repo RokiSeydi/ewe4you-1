43587939f3a9876a6f585946ae878dc5b67c456a
rokiseydi <roki.seydi@gmail.com>
2020-07-08 17:29:46 +0100

    deleted unused code

--- src/Components/NavigationBar/NavigationBar.jsx
+++ src/Components/NavigationBar/NavigationBar.jsx
@@ -22,21 +22,6 @@ iconClick = () => {
   render() {
     const display = this.state.isOpen ? null : styles.visible 
    
-    // (<nav>
-    //   <div className={styles.both}>
-    //   <div className={styles.links}>
-    //     <p><span><FontAwesomeIcon icon="tachometer-alt" /><Link to="/dashboard">Dashboard</Link></span></p>
-    //     <p><span><FontAwesomeIcon icon="comments" /><Link to="/topic-overview">Topics</Link></span></p>
-    //     <p><span><FontAwesomeIcon icon="wrench" /><Link to="/tools">Toolbox</Link></span></p>
-    //   </div>
-    //   <div className={styles.account}>
-    //     <p><span><FontAwesomeIcon icon="user"/><Link to="/myaccount">My Account</Link></span></p>
-    //    </div>
-    //   </div>
-    // </nav>  )
-
-      // ;
-
       return (
         <section className={styles.navbar} > 
           <img onClick={this.iconClick} src={logo} alt="sheep logo"/>            
