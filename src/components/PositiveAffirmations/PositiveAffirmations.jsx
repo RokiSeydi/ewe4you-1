09e174bb030b5513f38d946f762ed380c9187fad
mairead-hannah <mairead-hannah@github.com>
2020-07-08 17:05:42 +0100

    Added a second button with onclick to activate when a user inputs their sentences.

--- src/components/PositiveAffirmations/PositiveAffirmations.jsx
+++ src/components/PositiveAffirmations/PositiveAffirmations.jsx
@@ -4,6 +4,15 @@ import styles from "./PositiveAffirmations.module.scss";
 class PositiveAffirmations extends Component {
     state = {
         affirmations: [],
+        userInput: "",
+    }
+    
+    getAffirmations = (e) => {
+        const getInput = [...this.state.affirmations]
+        getInput.push(e.target.value);
+        this.setState({
+            userInput: e.target.value
+        })
     }
 
   UserText = () => {
@@ -14,15 +23,16 @@ class PositiveAffirmations extends Component {
         return ( 
             <>
             <h1>Positive Affirmations</h1>
-                <p>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Quia ipsum obcaecati, nam, soluta ipsam fugit magni commodi facilis qui deleniti facere dolorem alias, natus repellat ut tempore consequatur rerum. Quae.
+                <p>Please add 3 - 5 affirmations about yourself and/or your life. Lorem ipsum dolor sit amet consectetur, adipisicing elit. Quia ipsum obcaecati, nam, soluta ipsam fugit magni commodi facilis qui deleniti facere dolorem alias, natus repellat ut tempore consequatur rerum. Quae.
                     Lorem ipsum dolor sit, amet consectetur adipisicing elit. Pariatur fugiat voluptates quidem dolor magni! Quibusdam ea aperiam dolores, autem, aliquid et quia blanditiis cumque harum ab nobis hic veritatis provident!
                 </p>
             <section className={styles.toolPage}>
-                <input type="text"/>
-                <p>Lorem</p>
-                <p>Lorem</p>
-                <p>Lorem</p>
-                <button type="button" onClick={this.UserText}>Next steps...</button>
+                <input type="text" onInput={(e) => this.getAffirmations(e)}/>
+                <button onClick={this.getAffirmations}>Add</button>
+                <p>{this.userInput}</p>
+                <p>{}</p>
+                <p>{}</p>
+                <button onClick={this.UserText}>Next steps...</button>
                 <p></p>
             </section>
             </>
