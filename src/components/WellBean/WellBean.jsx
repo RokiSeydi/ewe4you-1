8d67659f320ca075095f92bf8ba3127538b1f16c
mel <melissa.sargent@hotmail.co.uk>
2020-07-08 16:34:14 +0100

    initial setup

---
+++ src/components/WellBean/WellBean.jsx
@@ -0,0 +1,14 @@
+import React, { Component } from 'react';
+
+class WellBean extends Component {
+  state = {  }
+  render() { 
+    return (
+      <>
+      <p>bean page works</p>
+      </>
+    );
+  }
+}
+ 
+export default WellBean;
