a17af53e6e8138727d5a05a7bf499400a6347f80
rokiseydi <roki.seydi@gmail.com>
2020-07-09 17:53:30 +0100

    fixed scss

--- src/Components/Navigation/BurgerMenu/BurgerMenu.jsx
+++ src/Components/Navigation/BurgerMenu/BurgerMenu.jsx
@@ -1,55 +1,21 @@
 import React, { Component } from 'react';
-import styled from "styled-components";
+import styles from "./BurgerMenu.module.scss";
 
 class BurgerMenu extends Component {
  
     render() { 
-     return (
-    <Wrapper onClick={this.props.handleNavbar}>
-      <div>
+     return (    
+   
+      <div className={styles.Wrapper} onClick={this.props.handleNavbar} >
         <span>&nbsp;</span>
         <span>&nbsp;</span>
         <span>&nbsp;</span>
       </div>
-    </Wrapper>
+  
    );
  }
 }   
    
 export default BurgerMenu;
-
-const Wrapper = styled.div`
-  position: relative;
-  padding-top: .7rem;
-  padding-right: 10px;
-  cursor: pointer;
-  display: block;
-  // margin-left: 10px;
-
-  & span {
-    background: #fdcb6e;
-    display: block;
-    position: relative;
-    width: 3.5rem;
-    height: .4rem;
-    margin-bottom: .7rem;
-    transition: all ease-in-out 0.2s;
-  }
-
-  .open span:nth-child(2) {
-      opacity: 0;
-    }
-
-  .open span:nth-child(3) {
-    transform: rotate(45deg);
-    top: -11px;
-  }
-
-  .open span:nth-child(1) {
-    transform: rotate(-45deg);
-    top: 11px;
-  }
-
-`;
 
 
