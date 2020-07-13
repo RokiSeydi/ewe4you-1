d34b4864ac178e6eb96a432c2c38a3cc53c6db7d
Aqeel Yamin <aqeel@Aqeels-MacBook-Pro.local>
2020-07-13 17:13:59 +0100

    instructions now appear in header

--- src/components/PositiveAffirmations/PositiveAffirmations.jsx
+++ src/components/PositiveAffirmations/PositiveAffirmations.jsx
@@ -42,6 +42,8 @@ class PositiveAffirmations extends Component {
             <NavigationBar/>
             <section className={styles.header}>.
             <h1>Positive Affirmations</h1>
+            {this.state.showIntructions || this.state.affirmations.length > 2 ? <p className={styles.showInstructionsStyle}> Repeat these 3 times to yourself in a mirror
+               You will help create a more positive narrative about yourself if you do this regularly. It will also help to create a more flexible and healthy view of yourself. </p> : ""}
                 <p>Please add 3 - 5 affirmations about yourself and/or your life.</p>
             </section>
             <section className={styles.toolPage}>
@@ -50,8 +52,6 @@ class PositiveAffirmations extends Component {
                 <button onClick={this.deleteAffirmations}>Delete</button>
                 <button onClick={this.updateInstructionsState}>Next</button>
                 {this.state.affirmations.map(affirmation => <p>{affirmation}</p>)}
-                {this.state.showIntructions || this.state.affirmations.length > 2 ? <p className={styles.showInstructionsStyle}> Repeat these 3 times to yourself in a mirror
-               You will help create a more positive narrative about yourself if you do this regularly. It will also help to create a more flexible and healthy view of yourself. </p> : ""}
             </section>
             </>
             
