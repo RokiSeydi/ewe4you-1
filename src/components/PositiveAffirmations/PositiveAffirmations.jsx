6fbb554e2bf145dcec500b1242d5bd3ee82d0804
Aqeel Yamin <aqeel@Aqeels-MacBook-Pro.local>
2020-07-13 17:05:10 +0100

    added third button to push next

--- src/components/PositiveAffirmations/PositiveAffirmations.jsx
+++ src/components/PositiveAffirmations/PositiveAffirmations.jsx
@@ -6,6 +6,7 @@ class PositiveAffirmations extends Component {
     state = {
         affirmations: [],
         userInput: "",
+        showInstructions: false,
     }
     
     getUserInput = (e) => {
@@ -21,15 +22,21 @@ class PositiveAffirmations extends Component {
             affirmations: updatedAffirmations
         })   }
     
-    showInstructions = () => {
-        return this.state.affirmations.length > 2 ? <p className={styles.showInstructionsStyle}> Repeat these 3 times to yourself in a mirror
-               You will help create a more positive narrative about yourself if you do this regularly. It will also help to create a more flexible and healthy view of yourself. </p> : null;
+    deleteAffirmations = () => {
+        const deleteAffirmations = [...this.state.affirmations]
+        deleteAffirmations.pop(this.state.userInput)
+        this.setState({
+            affirmations: deleteAffirmations
+        })
+    }
+    
+    updateInstructionsState = () => {
+        this.setState({
+            showInstructions: true
+        })
     }
 
-
     render() { 
-
-    
         return ( 
             <>
             <NavigationBar/>
@@ -40,8 +47,11 @@ class PositiveAffirmations extends Component {
             <section className={styles.toolPage}>
                 <input type="text" onInput={(e) => this.getUserInput(e)}/>
                 <button onClick={this.updateAffirmations}>Add</button>
+                <button onClick={this.deleteAffirmations}>Delete</button>
+                <button onClick={this.updateInstructionsState}>Next</button>
                 {this.state.affirmations.map(affirmation => <p>{affirmation}</p>)}
-            {this.showInstructions()}
+                {this.state.showIntructions || this.state.affirmations.length > 2 ? <p className={styles.showInstructionsStyle}> Repeat these 3 times to yourself in a mirror
+               You will help create a more positive narrative about yourself if you do this regularly. It will also help to create a more flexible and healthy view of yourself. </p> : ""}
             </section>
             </>
             
