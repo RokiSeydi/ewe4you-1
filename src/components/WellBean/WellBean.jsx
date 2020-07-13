8aa6beecee67afb2e1b1220bcc46c3a9debb8c47
Oli Black <oli.black@nology.io>
2020-07-13 15:15:50 +0100

    moving work to new branch to update master

---
+++ src/components/WellBean/WellBean.jsx
@@ -0,0 +1,92 @@
+import React, { Component } from 'react';
+import styles from "./WellBean.module.scss"
+
+import wellbeans from "../../data/wellbeans";
+import img from "../../assets/wellbeans/bakedbean.png"
+
+class WellBean extends Component {
+  state = { 
+    onBean: false,
+    currentBean: "hello",
+    otherBeans: []
+   }
+
+   clickedBean = (bean) =>{
+    this.setState({
+      onBean: !this.state.onBean,
+      currentBean: bean 
+    })
+    this.shuffleBeans(bean);
+  }
+
+  shuffleBeans = (selectedBean) => {
+    let otherBeanArray = [];
+    wellbeans.map((bean) => {
+      if(selectedBean.Id !== bean.Id){
+        otherBeanArray.push(bean);
+      }
+    })
+    this.setState({
+      otherBeans: otherBeanArray
+    })
+  }
+
+  otherBeanClick = (newBean) => {
+    this.shuffleBeans(newBean);
+    this.setState({
+      currentBean: newBean
+    })
+  }
+
+  render() {
+    if (this.state.onBean) {
+      return (
+        <>
+        <section className={styles.onBeanSection}>
+          <div className={styles.otherBeans}>
+            <div className={styles.beanImageContainer}><img src={this.state.otherBeans[0].image} onClick={() => this.otherBeanClick(this.state.otherBeans[0])} alt="bean"/></div>
+            <div className={styles.beanImageContainer}><img src={this.state.otherBeans[1].image} onClick={() => this.otherBeanClick(this.state.otherBeans[1])} alt="bean"/></div>
+            <div className={styles.beanImageContainer}><img src={this.state.otherBeans[2].image} onClick={() => this.otherBeanClick(this.state.otherBeans[2])} alt="bean"/></div>
+          </div>
+          <div className={styles.currentBean}>
+            <div className={styles.currentBeanImage}>
+              <img src={this.state.currentBean.image} alt="bean"></img>
+            </div>
+            <div className={styles.currentBeanList}>
+              <h3>{this.state.currentBean.text}</h3>
+              <div className ={styles.feelingListSection}>
+                <p>feeling 1</p>
+                <p>feeling 2</p>
+                <p>feeling 3</p>
+                <p>feeling 4</p>
+                <p>feeling 5</p>
+              </div>
+              <div></div>
+            </div>
+          </div>
+        </section>
+        </>
+      );
+    } else{
+      return (
+        <>
+        <div className ={styles.noBeanContainer}>
+          <h1>Pick a bean</h1>
+          <section className={styles.allbeans}>
+            <div className ={styles.beanRow}>          
+              <div className={styles.beanDiv}><img src={wellbeans[0].image} onClick={() => this.clickedBean(wellbeans[0])} alt="bean"/></div>
+              <div className={styles.beanDiv}><img src={wellbeans[1].image} onClick={() => this.clickedBean(wellbeans[1])} alt="bean"/></div>
+            </div>
+            <div className ={styles.beanRow}>
+              <div className={styles.beanDiv}><img src={wellbeans[2].image} onClick={() => this.clickedBean(wellbeans[2])} alt="bean"/></div>
+              <div className={styles.beanDiv}><img src={wellbeans[3].image} onClick={() => this.clickedBean(wellbeans[3])} alt="bean"/></div>
+            </div>
+          </section>
+        </div>
+        </>
+      );
+    }
+  }
+}
+ 
+export default WellBean;
