37c7f0d2106871129f8d3626e213c45cd7440bb8
Aqeel Yamin <aqeel@Aqeels-MacBook-Pro.local>
2020-07-14 15:08:28 +0100

    styling changes to input area

--- src/components/PositiveAffirmations/PositiveAffirmations.jsx
+++ src/components/PositiveAffirmations/PositiveAffirmations.jsx
@@ -42,11 +42,11 @@ class PositiveAffirmations extends Component {
             <NavigationBar/>
             <section className={styles.header}>.
             <h1>Positive Affirmations</h1>
-            {this.state.showIntructions || this.state.affirmations.length > 2 ? <p className={styles.showInstructionsStyle}> Repeat these 3 times to yourself in a mirror
+                {this.state.showIntructions || this.state.affirmations.length > 2 ? <p className={styles.showInstructionsStyle}> Great! Now that you've got these positive affirmations, repeat these 3 times to yourself in a mirror
                You will help create a more positive narrative about yourself if you do this regularly. It will also help to create a more flexible and healthy view of yourself. </p> : ""}
-                <p>Please add 3 - 5 affirmations about yourself and/or your life.</p>
             </section>
             <section className={styles.toolPage}>
+            <p>Please add 3 - 5 affirmations about yourself or your life.</p>
                 <input type="text" onInput={(e) => this.getUserInput(e)}/>
                 <button onClick={this.updateAffirmations}>Add</button>
                 <button onClick={this.deleteAffirmations}>Delete</button>
