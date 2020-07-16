5c087c6414b03776f2c9e8d9685f86d63186a74d
Oli Black <oli.black@nology.io>
2020-07-16 15:35:47 +0100

    test files update

--- src/containers/WellBeanContainer/WellBeanContainer.test.js
+++ src/containers/WellBeanContainer/WellBeanContainer.test.js
@@ -1,4 +1,7 @@
+import React from 'react';
+import { render } from '@testing-library/react';
+import WellBeanContainer from './WellBeanContainer';
 
 test('should render', () => {
-    expect(true).toBeTruthy();
+    expect(render( < WellBeanContainer / > )).toBeTruthy();
 });
