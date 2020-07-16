7cd3dfe647f3201c7747ad2da4e154eac91d6b8c
Aqeel Yamin <aqeel@Aqeels-MacBook-Pro.local>
2020-07-16 16:23:07 +0100

    got rid of test

--- src/components/PositiveAffirmations/PositiveAffirmations.test.js
+++
@@ -1,7 +0,0 @@
-import React from "react";
-import  PositiveAffirmations from "./PositiveAffirmations";
-import { render } from '@testing-library/react';
-
-test('should render', () => {
-  expect(render(<PositiveAffirmations />)).toBeTruthy();
-});
