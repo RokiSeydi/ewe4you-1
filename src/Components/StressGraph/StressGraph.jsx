d7c4a5896bde816024d79596c655075a2462e2c3
rokiseydi <roki.seydi@gmail.com>
2020-07-20 15:00:48 +0100

    work in progress

--- src/Components/StressGraph/StressGraph.jsx
+++ src/Components/StressGraph/StressGraph.jsx
@@ -1,6 +1,7 @@
 import React, { Component } from 'react';
 import styles from "./StressGraph.module.scss";
 import { PieChart } from "react-minimal-pie-chart";
+import { firestore } from "../../firebase.js";
 
 import NavigationBar from "../../components/Navigation/NavigationBar";
 
@@ -12,35 +13,54 @@ class StressGraph extends Component {
   life: 1
   }
 
+//   addToDatabase = () => {
+//     firestore
+//         .collection("StressGraph")
+//         .doc("Stressors")
+//         .set({ relationship: this.state.affirmations })
+//         .set({ work: this.state.work })
+//         .set({ life: this.state.life })
+//         .then((res) => console.log(res))
+//         .catch((err) => console.log(err))
+// }
 
-  getGraphJsx = () => {
-    let  pieChart = <div className={styles.graph}><PieChart
-        data={[
-          {
-            title: "Life",
-            value: this.state.life,
-            color: "#E38627",
-          },
-          {
-            title: "Relationship",
-            value: this.state.relationship,
-            color: "#C13C37",
-          },
-          {
-            title: "Work",
-            value: this.state.work,
-            color: "#6A2135",
-          },
-        ]}
-      /></div>;
 
-    for (const answer in this.state) {
-      if (answer === 0 || answer === "") pieChart = <p> Fill in the answers</p>;
-    }  
+// componentDidMount() {
+//   firestore
+//       .collection("StressGraph")
+//       .doc("Stressors")
+//       .get()
+//       .then((doc) => {
+//           const savedRelationship = doc.data().relationship;
+//           this.setState({ relationship: savedRelationship });
+//       })
+//       .then((doc) => {
+//         const savedWork = doc.data().work;
+//         this.setState({ work: savedWork });
+//       })
+//       .then((doc) => {
+//       const savedLife = doc.data().life;
+//       this.setState({ life: savedLife });
+//   })
+// }
 
-    return pieChart;
-  }
-
+// updateInput = () => {
+//   const updatedInput = [...this.state.relationship, ...this.state.work, ...this.state.life]
+//   updatedInput.push(this.state.relationship)
+//   this.setState({
+//       relationship: updatedInput
+//   })
+  
+//   updatedInput.push(this.state.work)
+//   this.setState({
+//       work: updatedInput
+//   })
+ 
+//   updatedInput.push(this.state.life)
+//   this.setState({
+//       life: updatedInput
+//   })
+// }
 
   render() { 
     return (
@@ -50,7 +70,7 @@ class StressGraph extends Component {
           <div className={styles.outer}>
             <div className={styles.inner}>
               <h2>Living Stress Graph</h2>
-            {/* {this.getGraphJsx()} */}
+           
               <div className={styles.graph}><PieChart
                 data={[
                   {
@@ -70,32 +90,33 @@ class StressGraph extends Component {
                   },
                 ]}
               /></div>
+
+
               <div className={styles.inputContainer}>
+
                 <p>What causes you the most stress?</p>
-                <input
-                  type="number"
-                  placeholder="e.i life"
-                  onChange={(e) =>
-                    this.setState({ life: parseInt(e.target.value) })
-                  }
-                />
-                <input
-                  type="number"
-                  placeholder="e.i relationship"
-                  onChange={(e) =>
-                    this.setState({
-                      relationship: parseInt(e.target.value),
-                    })
-                  }
-                />
-                <input
-                  type="number"
-                  placeholder="e.i work"
-                  onChange={(e) =>
-                    this.setState({ work: parseInt(e.target.value) })
-                  }
-                />
+
+                <form className={styles.form}>
+
+                  <input type="radio" value="Relationship" id="relationship"
+                   onChange={this.handleChange} name="gender" />
+                  <label for="male">Relationship</label>
+
+                  <input type="radio" value="Work" id="Work"
+                  onChange={this.handleChange} name="gender"/>
+                  <label for="female">Work</label>
+
+                  <input type="radio" value="fLife" id="Life"
+                  onChange={this.handleChange} name="gender"/>
+                  <label for="female">Life</label>
+
+                  <button className={styles.btn}>Submit</button>
+
+                </form>
+
               </div>
+
+
             </div>
           </div>
         </div>
