0cc0a593825c125d87cc3b39781169dd4328b284
Aqeel Yamin <aqeel@Aqeels-MacBook-Pro.local>
2020-07-23 12:44:44 +0100

    deleted nav

--- src/containers/TopicConclusionContainter/TopicConclusionContainter.jsx
+++ src/containers/TopicConclusionContainter/TopicConclusionContainter.jsx
@@ -1,7 +1,7 @@
 import React, { Component } from "react";
 import TopicConclusion from "../../components/TopicConclusion";
 import styles from "./TopicConclusionContainter.module.scss";
-import NavigationBar from "../../components/Navigation/NavigationBar";
+
 
 
 import topics from "../../data/topics";
@@ -10,7 +10,7 @@ class TopicConclusionContainter extends Component {
   render() {
     return (
       <>
-        <NavigationBar />
+    
         <div className={styles.Flexed}>
           <TopicConclusion topic={topics[0]} />
         </div>
