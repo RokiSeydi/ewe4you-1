21afc51042e1e1549bd5608aa32e7a94eeaf0afe
rokiseydi <roki.seydi@gmail.com>
2020-07-23 09:59:39 +0100

    deleted test.js

--- src/components/StressGraph/StressGraph.test.js
+++
@@ -1,7 +0,0 @@
-import React from "react";
-import StressGraph from "./StressGraph";
-import { render } from '@testing-library/react';
-
-test('should render', () => {
-    expect(render( < StressGraph / > )).toBeTruthy();
-});
